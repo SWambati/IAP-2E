6ff96b9a2b4f60e6c4401cdb97cd0733ecb60328
Wambati <165009303+Wambati@users.noreply.github.com>
2024-11-16 04:39:36 +0300

    Sign up and login with 2FA

--- SignUp/login.php
+++ SignUp/login.php
@@ -1,8 +1,3 @@
-<?php
-require "constants.php";
-require "dbConnection.php";
-$conn = new dbConn(DBTYPE, HOSTNAME, DBPORT, HOSTUSER, HOSTPASS, DBNAME);
-?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -11,12 +6,12 @@ $conn = new dbConn(DBTYPE, HOSTNAME, DBPORT, HOSTUSER, HOSTPASS, DBNAME);
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="style.css">
-        <title>Sign Up</title>
+        <title>Login</title>
     </head>
     <body>
         <div class="signup-container">
-            <form action="signup2.php" method="POST">
-              <h1>Sign Up</h1>
+            <form action="login2.php" method="POST">
+              <h1>Login</h1>
                 <div class="input">
                   <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="email address" name="email" required>
                   <i class="fa fa-user"></i>
@@ -26,16 +21,7 @@ $conn = new dbConn(DBTYPE, HOSTNAME, DBPORT, HOSTUSER, HOSTPASS, DBNAME);
                   <input type="password" class="form-control" id="exampleInputPassword1" placeholder="password"name="password" required>
                   <i class="fa fa-lock"></i>
                 </div>
-                <div class="input">
-                 
-                  <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Confirm password" name="confirm_password" required>
-                  <i class="fa fa-lock"></i>
-                </div>
-                <div class="remember-me">
-                  <label><input type="checkbox">Remember me</label>
-                  <label>Already have an account?</label><a href="login.php">Login</a>
-                </div>
-                <button type="submit" name="submit" class="btn">Sign Up</button>
+                <button type="submit" name="submit" class="btn">Login</button>
               </form>
 
         </div>
