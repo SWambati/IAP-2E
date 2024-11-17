514fdfc15119eafc55a716216b37d7581b9e0a41
Wambati <165009303+Wambati@users.noreply.github.com>
2024-11-17 20:28:43 +0300

    PHPMailer Plugin Test

--- email.php
+++ email.php
@@ -7,12 +7,12 @@ $mail = new PHPMailer(true);
 $mail->isSMTP();
 $mail->Host = 'smtp.gmail.com';
 $mail->SMTPAuth = true;
-$mail->Username = 'bibo.jimbo21@gmail.com';
-$mail->Password = 'tcrexjpdovcatycv';
-$mail->SMTPSecure = 'tls';
+$mail->Username = 'example@gmail.com';
+$mail->Password = '';
+$mail->SMTPSecure = 'tls'; 
 $mail->Port = 587;
 
-$mail->setFrom('bib0.jimbo21@gmail.com', 'IAP2E');
+$mail->setFrom('example@gmail.com', 'IAP2E');
 $mail->addAddress('example@gmail.com');
 $mail->Subject = 'Test Email';
 $mail->Body = 'This email tests the use of PHPMailer plugin.';
